5780bfffba975a1a3368002fe08ae0167d998bf8
ATHIBET <athibet@home.co.th>
2021-07-11 21:39:24 +0700

    update

---
+++ build/index.es.js
@@ -0,0 +1,61 @@
+import * as React from 'react';
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+***************************************************************************** */
+/* global Reflect, Promise */
+
+var extendStatics = function(d, b) {
+    extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+    return extendStatics(d, b);
+};
+
+function __extends(d, b) {
+    if (typeof b !== "function" && b !== null)
+        throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+    extendStatics(d, b);
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+}
+
+/**
+ * @class ExampleComponent
+ */
+var ExampleComponent = /** @class */ (function (_super) {
+    __extends(ExampleComponent, _super);
+    function ExampleComponent() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ExampleComponent.prototype.render = function () {
+        var text = this.props.text;
+        return React.createElement("div", { style: { color: "red", background: "green" } },
+            "Hello test ",
+            text,
+            " naja");
+    };
+    return ExampleComponent;
+}(React.Component));
+
+var ProjectCardVertical = function (_a) {
+    var title = _a.title;
+    return (React.createElement("div", null,
+        React.createElement("h1", null,
+            "My Title :",
+            title)));
+};
+
+export { ExampleComponent as Hello, ProjectCardVertical };
+//# sourceMappingURL=index.es.js.map
